655d66b787768ce8077c49c928401c150dd0985f
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-07-14 16:59:10 +1000

    config commit

---
+++ src/Login/LoginPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import TextInput from '../components/TextInput'
+
+const LoginPage = props => (
+  <div>
+    <h2>Login Page</h2>
+    <form>
+      <TextInput
+        id="username"
+        label="username"
+        onChange={props.onChange}
+        name="username"
+        value="props.user.username"
+      />
+      <TextInput
+        id="password"
+        label="password"
+        onChange={props.onChange}
+        name="password"
+        value="props.user.password"
+      />
+      <input type="submit" value="save" className="btn btn-primary" />
+    </form>
+  </div>
+)
+
+export default LoginPage
